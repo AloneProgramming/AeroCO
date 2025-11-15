9b93cedd3e6033e03fba2f12295445abed61b066
AloneProgramming <dimackuda@gmail.com>
2025-11-15 10:33:18 +0800

    Some fixes

--- demo/demo_advanced.py
+++ demo/demo_advanced.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     airfoil_test = DiscreteVortexMethod(
         alpha_degrees=5,
         n_panels=200,
-        camber_func=naca_4_digit_camber(code="9412"),
+        camber_func=naca_4_digit_camber(code="4412"),
         debug=True)
 
     airfoil_data(airfoil=airfoil_test, types=("CL", "CM_LE", "CM_AC",), alpha_range=(0, 15, 3))
